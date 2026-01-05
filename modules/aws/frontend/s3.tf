768fe51ccaee9f9ce1fd2c978f8366d5e7a1b196
Chukwudi Chima <brianchima22@gmail.com>
2026-01-05 23:20:20 +0100

    added cloudfront config file, updated s3 config to only allow read access from cloudfront

--- modules/aws/frontend/s3.tf
+++ modules/aws/frontend/s3.tf
@@ -19,15 +19,6 @@ resource "aws_s3_bucket_website_configuration" "website" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "frontend" {
-  bucket = aws_s3_bucket.frontend.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
 resource "aws_s3_bucket_policy" "frontend" {
   bucket = aws_s3_bucket.frontend.id
 
@@ -35,11 +26,18 @@ resource "aws_s3_bucket_policy" "frontend" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "PublicReadGetObject"
+        Sid       = "CloudFrontObjectReadOnly"
         Effect    = "Allow"
-        Principal = "*"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
         Action    = "s3:GetObject"
         Resource  = "${aws_s3_bucket.frontend.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.frontend-cdn.arn
+          }
+        }
       }
     ]
   })
