04950436d5269ce9691e4a0a0d5e1da9a2009215
Chukwudi Chima <brianchima22@gmail.com>
2026-03-13 12:03:54 +0100

    added storage database(rds postgres, dynamo db) and more robust logging

---
+++ infrastructure/modules/aws/storage/dynamo.tf
@@ -0,0 +1,22 @@
+resource "aws_dynamodb_table" "dynamo_db" {
+  name = "${var.project_name}-dynamo-db"
+  billing_mode = "PROVISIONED"
+  read_capacity = 5
+  write_capacity = 5
+  hash_key = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled = true
+  }
+
+  replica {
+    region_name = "eu-east-1"
+    consistency_mode = "EVENTUAL"
+  }
+}
